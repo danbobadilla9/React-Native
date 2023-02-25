03186311d173930294f11710dac1cf8055527f96
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-24 23:34:27 -0600

    Terminado Contador y StyleShet agregado

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Button, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
 export const ContadorScreen = () => {
@@ -7,31 +7,63 @@ export const ContadorScreen = () => {
     const [contador, setContador] = useState(10);
 
     return (
-        <View style={
-            {
-                flex: 1,
-                justifyContent: 'center'
-            }
-        }>
-            <Text style={
-                {
-                    textAlign: 'center',
-                    fontSize: 40,
-                    top: -10
-                }
-            }>Contador: {contador}</Text>
+        <View style={ styles.container }>
+            <Text style={ styles.title }>Contador: {contador}</Text>
 
             <TouchableOpacity
+                style={styles.fabLocationBR}
                 onPress={ () => setContador(contador + 1)}
             >
-                <View style={{
-                    backgroundColor: 'red',
-                    borderRadius: 100,
-                }}>
-                    <Text>+1</Text>
+                <View style={ styles.fab}>
+                    <Text style={styles.fabText}>+1</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.fabLocationBL}
+                onPress={ () => setContador(contador - 1)}
+            >
+                <View style={ styles.fab}>
+                    <Text style={styles.fabText}>-1</Text>
                 </View>
             </TouchableOpacity>
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -10
+    },
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+    fab: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 50,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+
+    }
+})
