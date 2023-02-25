dab593dd44db25e5001a76f46feec8671845926f
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-25 17:24:35 -0600

    Terminando el primer proyecto

--- 02-MiPrimeraApp/src/components/Fab.tsx
+++ 02-MiPrimeraApp/src/components/Fab.tsx
@@ -1,47 +1,70 @@
 import React from 'react'
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
 
-interface Props{
+interface Props {
     title: string,
     position?: 'br' | 'bl',
     onPress: () => void,
 
 }
 
-export const Fab = ({ title, onPress,position = 'br' }: Props) => {
+export const Fab = ({ title, onPress, position = 'br' }: Props) => {
 
     console.log(title);
-    
+
     return (
-        <TouchableOpacity
-                style={styles.fabLocationBR}
-                onPress={ onPress }
+        <View
+            style={[
+                // ( position === 'bl') ? styles.fabLocationBL: styles.fabLocationBR
+                styles.fabLocation,
+                (position === 'bl') ? styles.left : styles.right
+            ]}
+        >
+            <TouchableNativeFeedback
+                onPress={onPress}
+                background= { TouchableNativeFeedback.Ripple('#28425B',false,30) }
             >
-                <View style={ styles.fab}>
-                    <Text style={styles.fabText}>{ title }</Text>
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>{title}</Text>
                 </View>
-            </TouchableOpacity>
+            </TouchableNativeFeedback>
+        </View>
+
 
     )
 }
 const styles = StyleSheet.create({
-    fabLocationBR: {
+    fabLocation: {
         position: 'absolute',
         bottom: 25,
-        right: 25
+
     },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
-        left: 25
-    },
+    // fabLocationBR: {
+    //     position: 'absolute',
+    //     bottom: 25,
+    //     right: 25
+    // },
+    // fabLocationBL: {
+    //     position: 'absolute',
+    //     bottom: 25,
+    //     left: 25
+    // },
     fab: {
         backgroundColor: '#5856D6',
         width: 60,
         height: 60,
         borderRadius: 100,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+
+        elevation: 10,
     },
     fabText: {
         color: 'white',
@@ -49,5 +72,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         alignSelf: 'center',
 
-    }
+    },
+    right: {
+        right: 25
+    },
+    left: {
+        left: 25
+    },
 })
